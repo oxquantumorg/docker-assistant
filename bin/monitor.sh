f05d40bff2449d207a229c06f1452e850e06a733
bigchiano <bigchiano@protonmail.com>
2024-08-09 00:51:32 +0300

    add server name to logs

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -4,20 +4,21 @@
 source "$(dirname "$0")/../lib/telegram.sh"
 source "$(dirname "$0")/../config/app.conf"
 
-message=''
+message="Message from: $SERVER_NAME
+"
 err_message=''
 for CONTAINER_NAME in ${CONTAINERS[@]}; do
     if ! docker ps | grep $CONTAINER_NAME > /dev/null; then
-        err_message="${err_message}______________________________________________
-" 
-        err_message="${err_message}-Container $CONTAINER_NAME is not running. Restarting... 
+err_message="${err_message}______________________________________________
+"
+err_message="${err_message}-Container $CONTAINER_NAME is not running. Restarting...
 "
         docker restart "$CONTAINER_NAME"
         if [ $? -eq 0 ]; then
-            err_message+="Successfully restarted $CONTAINER_NAME. 
+err_message+="Successfully restarted $CONTAINER_NAME.
 "
         else
-            err_message+="Failed to restart $CONTAINER_NAME. 
+err_message+="Failed to restart $CONTAINER_NAME.
 "
         fi
     else
