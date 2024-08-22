5c80aefd3cc67ef0024bc0fa2f570ca31f852e0b
bigchiano <bigchiano@protonmail.com>
2024-08-22 21:50:37 +0300

    permit all files

--- start-bash.sh
+++ start-bash.sh
@@ -4,5 +4,5 @@
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # Change permissions and run monitor-bash.sh using the dynamic path
-chmod 777 "$SCRIPT_DIR/bin/monitor-bash.sh"
+chmod 777 "$SCRIPT_DIR"
 "$SCRIPT_DIR/bin/monitor-bash.sh"
